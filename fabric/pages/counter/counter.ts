f757c011bf83e1aa0147d1727627be19a73aef88
wt <3050540005@qq.com>
2025-09-11 15:59:35 +0800

    fix: 屏幕常亮状态下，手动熄屏后，再次打开屏幕，屏幕不常亮问题

--- fabric/pages/counter/counter.ts
+++ fabric/pages/counter/counter.ts
@@ -128,6 +128,7 @@ Page({
   },
 
   onShow() {
+    this.initKeepScreen();
     if (typeof this.getTabBar === "function" && this.getTabBar()) {
       this.getTabBar().setData({
         selected: 1,
