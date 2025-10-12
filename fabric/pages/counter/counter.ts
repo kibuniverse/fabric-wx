8fb8c427c48d55a737de00ea984dbd25d4fe6639
kibuniverse <kaikizizhi@gmail.com>
2025-10-12 22:11:38 +0800

    fix: tab name sync fail

--- fabric/pages/counter/counter.ts
+++ fabric/pages/counter/counter.ts
@@ -599,6 +599,17 @@ Page({
       return;
     }
 
+    // 修改计数器名称
+    const newCounterKeys = this.data.counterKeys.map((item) => {
+      if (item.key === this.data.currentEditingKey) {
+        return { ...item, title: newName };
+      }
+      return item;
+    });
+    this.setData({
+      counterKeys: newCounterKeys,
+    });
+    wx.setStorageSync(STORAGE_KEYS.COUNTER_KEYS, newCounterKeys);
     const counterData = wx.getStorageSync(this.data.currentEditingKey);
     if (counterData) {
       counterData.name = newName;
