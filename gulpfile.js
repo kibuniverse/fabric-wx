ceaf1db173e2d2fb52c08bae44984e56af55657d
wt <3050540005@qq.com>
2026-03-07 21:28:52 +0800

    修复UI问题

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@ const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const pxtounits = require('postcss-px2units');
 
-gulp.task('css', () => {
-    return gulp.src(['fabric/components/counter/*.wxss'])
-        .pipe(postcss([pxtounits({
-            multiple: 2, // 1px = 2rpx
-            targetUnits: 'rpx'
-        })]))
-        .pipe(gulp.dest('fabric/components/counter/'));
+// gulp.task('css', () => {
+//     return gulp.src(['fabric/components/counter/*.wxss'])
+//         .pipe(postcss([pxtounits({
+//             multiple: 2, // 1px = 2rpx
+//             targetUnits: 'rpx'
+//         })]))
+//         .pipe(gulp.dest('fabric/components/counter/'));
+// });
+gulp.task('css', function () {
+  return gulp.src(['fabric/miniprogram_npm/@vant/weapp/**/*.wxss'])
+      .pipe(postcss([pxtounits({
+          multiple: 2,
+          targetUnits: 'rpx'
+      })]))
+      .pipe(gulp.dest('fabric/miniprogram_npm/@vant/weapp/'));
 });
